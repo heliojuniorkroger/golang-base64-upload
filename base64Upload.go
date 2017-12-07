63eb6de6a656ae3398fdb313285f324e98e7e9b4
Hélio Júnior Rodrigues Kroger <heliojuniorrodrigueskroger@Mac-mini-de-TI.local>
2017-12-07 14:33:08 -0200

    Return at the end of the function

--- base64Upload.go
+++ base64Upload.go
@@ -2,22 +2,13 @@ package base64Upload
 
 import (
       "os"
-      "io"
       "encoding/base64"
 )
 
 func Upload(fileName string, content string) error {
       decode, err := base64.StdEncoding.DecodeString(content)
-      if err != nil {
-            return err
-      }
       file, err := os.Create(fileName)
-      if err != nil {
-            return err
-      }
       defer file.Close()
       _, err = file.Write(decode)
-      if err != nil {
-            return err
-      }
+      return err
 }
